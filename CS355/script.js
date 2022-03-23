e77bfec0321f3efa77e37e714f8d4385f5498d05
PAraujoo <78515402+PAraujoo@users.noreply.github.com>
2022-03-23 19:52:59 -0400

    Add files via upload

---
+++ CS355/script.js
@@ -0,0 +1,31 @@
+/*const icon = document.querySelector('.icon');
+const search = document.querySelector('.search');
+icon.onclick = function();
+    search.classList.toggle('active'); */
+
+let searchBar = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () => {
+    searchBar.classList.toggle('active');
+}
+
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach((panels) => {
+    panels.addEventListener('click', () => {
+        removeActiveClasses()
+        panels.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panels => {
+        panels.classList.remove('active')
+    })
+}
+
+let menuBar = document.querySelector('.menu-form');
+
+document.querySelector('#menu-btn').onclick = () => {
+    menuBar.classList.toggle('active');
+}
